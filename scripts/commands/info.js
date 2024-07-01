c2e33dcf23fd9d723f13b48b2ea9e9ffbfc5ba44
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-07-01 11:36:12 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -25,33 +25,23 @@ const time = process.uptime(),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg", 
-            
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg", 
-            
-            "https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
-
-"https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
-            
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg"];
+var link = ["https://i.imgur.com/SQTANmV.jpeg"];
   
 var callback = () => api.sendMessage({body:` 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
 =======================
 𝗕𝗢𝗧 𝗡𝗔𝗠𝗘 : ${global.config.BOTNAME}
-𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 :✢ 𝗡𝗔𝗭𝗥𝗨𝗟✢
-𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
+𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝐌𝐈𝐊𝐀𝐈𝐋
+𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝐒𝐀𝐓𝐊𝐇𝐈𝐑𝐀
 =======𝗖𝗢𝗡𝗧𝗔𝗖𝗧=======
 =======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100050059535072
 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
+https://m.me/100050059535072
 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX}
-𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: জিঁলাঁপিঁ
+𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝐌𝐈𝐊𝐀𝐈𝐋
 𝗢𝗧𝗛𝗘𝗥 𝗡𝗘𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡 ==============================================
 𝗧𝗢𝗗𝗬 𝗜𝗦 𝗧𝗜𝗠𝗘 : ${juswa} 
 𝗕𝗢𝗧 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚 ${hours}:${minutes}:${seconds}.
-𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗨𝗦𝗜𝗡𝗚 ${global.config.BOTNAME} \n=======================
-✢━━━━━ N/S ━━━━━✢
-----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖---- `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗨𝗦𝗜𝗡𝗚 ${global.config.BOTNAME}  `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
