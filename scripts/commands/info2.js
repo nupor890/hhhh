39d16b91cc7f6fec1615527db21f01e84720f29e
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-07-01 11:02:41 +0600

    Update info2.js

--- scripts/commands/info2.js
+++ scripts/commands/info2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     permission: 0,
-    credits: "nayan",
+    credits: "nazrul",
     prefix: true,
     description: "",
     category: "prefix",
@@ -28,17 +28,15 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
 =======================
-❇️𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 :
-✢━ 𝗡𝗔𝗭𝗥𝗨𝗟 ━✢
-❇️𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
+𝗔𝗗𝗠𝗜𝗡 : 𝐌𝐈𝐊𝐀𝐈𝐋
+𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝐒𝐀𝐓𝐊𝐇𝐈𝐑𝐀
 =======𝗖𝗢𝗡𝗧𝗔𝗖𝗧=======
 =======================
-❇️𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
-❇ 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
-❇️𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: ✢ 𝗡𝗔𝗭𝗥𝗨𝗟 ✢
-❇️𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100050059535072
+𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
+https://m.me/100050059535072
+𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝐌𝐈𝐊𝐀𝐈𝐋`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000350435589/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100050059535072/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
